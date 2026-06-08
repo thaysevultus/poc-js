5ebde21c4b5a956ee08518c476461a44d73a8781
thaysevultus <thayse.solis@vultuscyber.com.br>
2026-06-08 15:22:59 -0300

    Create x.js

---
+++ x.js
@@ -0,0 +1,10 @@
+window.PWNED = true;
+
+console.log("MODULE EXECUTED");
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  "<div style='position:fixed;top:0;left:0;width:100%;background:red;color:white;padding:20px;z-index:999999'>POC EXECUTED</div>"
+);
+
+export default {};
