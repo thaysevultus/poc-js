7d0dbdc1ea1ffda350ced8260858397a43068090
thaysevultus <thayse.solis@vultuscyber.com.br>
2026-06-22 10:40:26 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-<button onclick="testarXHR()">Testar XHR</button>
-
-<script>
 function testarXHR() {
     var xhr = new XMLHttpRequest();
 
@@ -20,4 +17,33 @@ function testarXHR() {
     xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
     xhr.send();
 }
-</script>
+
+const btn = document.createElement("button");
+btn.textContent = "Testar XHR";
+btn.onclick = testarXHR;
+
+document.body.appendChild(btn);
+
+export {};
+
+Ou, se você só quer disparar o XHR assim que o módulo for carregado:
+
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        console.log("Status:", xhr.status);
+        console.log("Resposta:", xhr.responseText);
+
+        if (xhr.status === 200) {
+            alert("XHR funcionando!");
+        } else {
+            alert("Erro: " + xhr.status);
+        }
+    }
+};
+
+xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+xhr.send();
+
+export {};
