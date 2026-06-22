a0e4e36e54ebd5184ae322968fb5fbe1a60380eb
thaysevultus <thayse.solis@vultuscyber.com.br>
2026-06-22 10:43:32 -0300

    Update test.js

--- test.js
+++ test.js
@@ -23,27 +23,3 @@ btn.textContent = "Testar XHR";
 btn.onclick = testarXHR;
 
 document.body.appendChild(btn);
-
-export {};
-
-Ou, se você só quer disparar o XHR assim que o módulo for carregado:
-
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        console.log("Status:", xhr.status);
-        console.log("Resposta:", xhr.responseText);
-
-        if (xhr.status === 200) {
-            alert("XHR funcionando!");
-        } else {
-            alert("Erro: " + xhr.status);
-        }
-    }
-};
-
-xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
-xhr.send();
-
-export {};
